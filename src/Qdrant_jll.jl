9f3e85145c2e968a61eb1ea7ecc92505e537d4ee
jlbuild <juliabuildbot@gmail.com>
2026-04-27 23:27:09 +0000

    Qdrant_jll build 1.17.1+0

--- src/Qdrant_jll.jl
+++ src/Qdrant_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Qdrant")
-JLLWrappers.@generate_main_file("Qdrant", UUID("49d5a0a8-0cca-57b3-8548-a2cd8c16dcd0"))
+JLLWrappers.@generate_main_file("Qdrant", Base.UUID("49d5a0a8-0cca-57b3-8548-a2cd8c16dcd0"))
 end  # module Qdrant_jll
